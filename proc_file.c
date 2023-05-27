3b400056951f92435c66a80ae7d4b7f4bfcf4503
Bamansour Abdennebi <86577036+bamansour1978@users.noreply.github.com>
2023-05-27 01:46:06 +0100

    Update proc_file.c

--- proc_file.c
+++ proc_file.c
@@ -6,7 +6,7 @@ int proc_file_commands(char *file_path, int *exe_ret);
 /**
  * cant_open - If the file doesn't exist or lacks proper permissions, print
  * a cant open error.
- * @file_path: Path to the supposed file.
+ * @file_path: Path to the supposed file_.
  *
  * Return: 127.
  */
